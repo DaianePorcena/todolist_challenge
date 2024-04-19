737ab7c2c927633a67f8c98f69d9ab91f331b9e8
DaianePorcena <daiporcenna@gmail.com>
2024-04-19 11:04:24 -0300

    ✨ feat: Adiciona funcionalidade para adicionar uma tarefa e modal de edição

--- todo_frontend/src/pages/Home.js
+++ todo_frontend/src/pages/Home.js
@@ -1,38 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import TodoItem from "../components/TodoItem/TodoItem";
-import logoIcon from '../assets/logo.svg'
-import './Home.css'
+import AddTask from "../components/AddTask/AddTask";
+import logoIcon from '../assets/logo.svg';
+import './Home.css';
+
 const Home = () => {
     const [todos, setTodos] = useState([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     useEffect(() => {
+        fetchTodos();
+    }, []);
+
+    const fetchTodos = () => {
         axios.get('http://localhost:8080/tasks')
             .then(response => {
                 setTodos(response.data);
-                console.log(response.data)
-                console.log('teste');
             })
             .catch(error => {
                 console.error('Error fetching data:', error);
             });
-    }, []);
+    };
 
     const handleEdit = (todo) => {
         console.log('Edit todo:', todo);
-       
     };
 
     const handleDelete = (id) => {
-        console.log('Delete todo with ID:', id);
-        
+        axios.delete(`http://localhost:8080/tasks/${id}`)
+            .then(() => {
+                // Atualiza a lista de tarefas após deletar a tarefa
+                fetchTodos();
+                console.log('Tarefa deletada com sucesso');
+            })
+            .catch(error => {
+                console.error('Error deleting task:', error);
+            });
+    };
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+        fetchTodos(); // Atualiza a lista de tarefas após fechar o modal
     };
 
     return (
         <div className='home'>
             <div className='logo'>
-            <h2>Todo List</h2>
-            <img src={logoIcon} alt='logo'/>
+                <h1>Todo List</h1>
+                <img src={logoIcon} alt='logo'/>
             </div>
             
             <div className='container'>
@@ -45,13 +65,19 @@ const Home = () => {
                     />
                 ))}
 
-                <button className='addTask'>
+                <button className='addTask' onClick={openModal}>
                    + ADICIONAR TAREFA
                 </button>
+
+                {isModalOpen && <div className="modal">
+                    <div className="modal-content">
+                        {/* <span className="close" onClick={closeModal}>&times;</span> */}
+                        <AddTask onAdd={closeModal} onCancel={closeModal}/>
+                    </div>
+
+                    
+                </div>}
             </div>
-
-
-            
         </div>
     );
 }
