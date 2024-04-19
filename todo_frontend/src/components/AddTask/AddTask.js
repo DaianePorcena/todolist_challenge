d673dd5f67999c21c72fd38d70c7b8b160f15bdd
DaianePorcena <daiporcenna@gmail.com>
2024-04-19 12:22:59 -0300

    ✨ feat: Adiciona funcionalidade de edição de tarefa ao componente AddTask

--- todo_frontend/src/components/AddTask/AddTask.js
+++ todo_frontend/src/components/AddTask/AddTask.js
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './AddTask.css'
 import axios from 'axios';
 
-const AddTask = ({ onAdd, onCancel }) => {
+const AddTask = ({ onAdd, onCancel, selectedTodo }) => {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [status, setStatus] = useState('NAO_INICIADO'); // Estado para armazenar o status da tarefa
+
+    useEffect(() => {
+        if (selectedTodo) {
+            setTitle(selectedTodo.title);
+            setDescription(selectedTodo.description);
+            setStatus(selectedTodo.status);
+        }
+    }, [selectedTodo]);
 
     const handleTitleChange = (event) => {
         setTitle(event.target.value);
@@ -32,18 +40,32 @@ const AddTask = ({ onAdd, onCancel }) => {
             status: status // Usando o status selecionado pelo usuário
         };
 
-        axios.post('http://localhost:8080/tasks', newTask)
-            .then(response => {
-                // Chamando a função onAdd passada como prop para atualizar a lista de tarefas
-                onAdd(response.data);
-                // Resetando os campos do formulário após a adição da tarefa
-                setTitle('');
-                setDescription('');
-                setStatus('NAO_INICIADO'); // Resetando o status para o padrão
-            })
-            .catch(error => {
-                console.error('Error adding task:', error);
-            });
+        if (selectedTodo) {
+            // Função para verificar se está em modo de ediçã e enviar uma requisição PUT
+            axios.put(`http://localhost:8080/tasks/${selectedTodo.id}`, newTask)
+                .then(response => {
+                    onAdd(response.data);
+                    setTitle('');
+                    setDescription('');
+                    setStatus('NAO_INICIADO');
+                })
+                .catch(error => {
+                    console.error('Error updating task:', error);
+                });
+        } else {
+            // Caso contrário, uma requisição POST será enviada para adicionar uma nova tarefa
+            axios.post('http://localhost:8080/tasks', newTask)
+                .then(response => {
+                    onAdd(response.data);
+                    setTitle('');
+                    setDescription('');
+                    setStatus('NAO_INICIADO');
+                })
+                .catch(error => {
+                    console.error('Error adding task:', error);
+                });
+        }
+        
     };
 
     return (
