aaf538bc0f6dbd07851ffde5a8f97648ed938281
DaianePorcena <daiporcenna@gmail.com>
2024-04-19 06:45:55 -0300

    ✨ feat: Imporma a Home para AppRoutes

--- todo_frontend/src/routers/AppRoutes.js
+++ todo_frontend/src/routers/AppRoutes.js
@@ -1,6 +1,7 @@
 // AppRoutes.js
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";import Home from "../pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
 
 const AppRoutes = () => {
   return (
